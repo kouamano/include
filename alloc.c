67fc1c9dec0ff83e3484d820316093292e257500
kouamano <amano.au1@gmail.com>
2018-08-22 11:46:18 +0900

    rev

--- alloc.c
+++ alloc.c
@@ -5,11 +5,11 @@
 /* complex.h */
 
 /*************************************************/
-/* Copyright (c) 2003-2005 AMANO Kou             */
+/* Copyright (c) 2003-2017 AMANO Kou             */
+/* RIKEN                                         */
 /* University of Tsukuba                         */
 /* University of Liblary And Information Science */
 /* National Institute of Agrobilogical Sciences  */
-/* kamano@affrc.go.jp                            */
 /*************************************************/
 
 /**************************************************
@@ -271,11 +271,28 @@ double **d_calloc_mat(int num, int dim){
 	double **m;
 	int i;
 	if((m = calloc((size_t)num,(size_t)sizeof(double *))) == NULL){
-		fprintf(stderr,"[E]failed : calloc() at %ld byte.\n",(long int)sizeof(float *)*num);
+		fprintf(stderr,"[E]failed : calloc() at %ld byte.\n",(long int)sizeof(double *)*num);
 		exit(1);
 	}
 	if((m[0] = calloc((size_t)num*dim,(size_t)sizeof(double))) == NULL){
-		fprintf(stderr,"[E]failed : calloc() at %ld byte.\n",(long int)sizeof(float)*num*dim);
+		fprintf(stderr,"[E]failed : calloc() at %ld byte.\n",(long int)sizeof(double)*num*dim);
+		exit(1);
+	}
+	for(i=1;i<num;i++){
+		m[i] = m[i-1]+dim;
+	}
+	return(m);
+}
+
+complex **x_calloc_mat(int num, int dim){
+	complex **m;
+	int i;
+	if((m = calloc((size_t)num,(size_t)sizeof(complex *))) == NULL){
+		fprintf(stderr,"[E]failed : calloc() at %ld byte.\n",(long int)sizeof(complex *)*num);
+		exit(1);
+	}
+	if((m[0] = calloc((size_t)num*dim,(size_t)sizeof(complex))) == NULL){
+		fprintf(stderr,"[E]failed : calloc() at %ld byte.\n",(long int)sizeof(complex)*num*dim);
 		exit(1);
 	}
 	for(i=1;i<num;i++){

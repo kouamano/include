3c94792d96fb7c4fc63982e278c14cce62d09442
kouamano <amano.au1@gmail.com>
2019-07-12 11:27:27 +0900

    add

--- hash.c
+++ hash.c
@@ -19,6 +19,13 @@ struct hash {
     size_t table_size;
 };
 
+struct similarity_hash{
+        int node_s;
+        int node_e;
+        float sim;
+};
+
+
 static void err_exit(const char *str);
 static size_t hash_func(struct hash *hash, const char *key);
 static void *make_pair(struct hash* hash, const char *key, const void *value);
@@ -148,5 +155,23 @@ static void free_pair(struct pair *pair)
     pair->val = NULL;
 
     free(pair);
+}
+
+struct similarity_hash *f_alloc_similarity_hash(int num, struct similarity_hash *sim_hash){
+        if((sim_hash = malloc(sizeof(struct similarity_hash)*num)) == NULL){
+                fprintf(stderr,"[E] failed malloc().\n");
+                exit(1);
+        }
+        return(sim_hash);
+}
+
+int read_simmilarity_hash_from_stream(int num, FILE *IN, struct similarity_hash *sim_hash){
+        int i = 0;
+        char line[TMP_STR_LEN_LONG];
+        while(line,TMP_STR_LEN_LONG,IN){
+                sscanf(line,"%d %d %f",&(*(sim_hash+i)).node_s,&(*(sim_hash+i)).node_e,&(*(sim_hash+i)).sim);
+                i++;
+        }
+        return(i);
 }
 
